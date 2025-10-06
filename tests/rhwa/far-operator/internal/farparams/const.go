996a098a8767425ab8e43b52ac28a6e396e7cc2a
Francisco Javier Moreno <frajamomo@hotmail.com>
2025-10-06 10:35:00 +0200

    rhwa: Validate number of replicas of FAR controller (#810)
    * Validate number of replicas of FAR controller
    
    * Convert ExpectedReplicas into constant
    
    * Skip TC if SNO topology
    
    * Fix case ID

--- tests/rhwa/far-operator/internal/farparams/const.go
+++ tests/rhwa/far-operator/internal/farparams/const.go
@@ -3,4 +3,7 @@ package farparams
 const (
 	// Label represents far operator label that can be used for test cases selection.
 	Label = "far"
+
+	// ExpectedReplicas defines the expected number of replicas for FAR controller manager.
+	ExpectedReplicas = int32(2)
 )
--- tests/rhwa/far-operator/tests/far.go
+++ tests/rhwa/far-operator/tests/far.go
@@ -5,8 +5,10 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	configv1 "github.com/openshift/api/config/v1"
 
 	"github.com/rh-ecosystem-edge/eco-goinfra/pkg/deployment"
+	"github.com/rh-ecosystem-edge/eco-goinfra/pkg/infrastructure"
 	"github.com/rh-ecosystem-edge/eco-goinfra/pkg/pod"
 	"github.com/rh-ecosystem-edge/eco-goinfra/pkg/reportxml"
 
@@ -22,9 +24,12 @@ var _ = Describe(
 	Ordered,
 	ContinueOnFailure,
 	Label(farparams.Label), func() {
+		var farDeployment *deployment.Builder
+
 		BeforeAll(func() {
 			By("Get FAR deployment object")
-			farDeployment, err := deployment.Pull(
+			var err error
+			farDeployment, err = deployment.Pull(
 				APIClient, farparams.OperatorDeploymentName, rhwaparams.RhwaOperatorNs)
 			Expect(err).ToNot(HaveOccurred(), "Failed to get FAR deployment")
 
@@ -43,5 +48,24 @@ var _ = Describe(
 				listOptions,
 			)
 			Expect(err).ToNot(HaveOccurred(), "Pod is not ready")
+		})
+
+		It("Verify FAR controller manager has correct number of replicas", reportxml.ID("61222"), func() {
+			By("Checking cluster topology")
+			infraConfig, err := infrastructure.Pull(APIClient)
+			Expect(err).ToNot(HaveOccurred(), "Failed to pull infrastructure configuration")
+
+			if infraConfig.Object.Status.ControlPlaneTopology == configv1.SingleReplicaTopologyMode {
+				Skip("Skipping test on SNO (Single Node OpenShift) cluster")
+			}
+
+			By("Checking deployment replicas")
+			Expect(farDeployment.Object.Spec.Replicas).ToNot(BeNil(), "Deployment replicas should not be nil")
+			Expect(*farDeployment.Object.Spec.Replicas).To(Equal(farparams.ExpectedReplicas),
+				"Expected %d replica(s), found %d", farparams.ExpectedReplicas, *farDeployment.Object.Spec.Replicas)
+
+			By("Verifying ready replicas")
+			Expect(farDeployment.Object.Status.ReadyReplicas).To(Equal(farparams.ExpectedReplicas),
+				"Expected %d ready replica(s), found %d", farparams.ExpectedReplicas, farDeployment.Object.Status.ReadyReplicas)
 		})
 	})
